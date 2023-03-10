2d24520a82ae17da68b3b5fb2e2bd4ee820ea3a0
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-03-10 22:59:47 +0100

    clockClassAdded

---
+++ Clock.swift
@@ -0,0 +1,66 @@
+//
+//  Clock.swift
+//  chessClock
+//
+//  Created by Tobias Sörensson on 2023-03-10.
+//
+
+import Foundation
+import UIKit
+
+
+class Clock {
+    var secondsLeft : Int = 0
+    var startTime : Date?
+    var timer : Timer?
+    //let formatter : DateFormatter?
+    var timeSpent : Int
+    var isRunning : Bool
+    var countdown : UILabel!
+    var timeEdit: UITextField!
+    
+    init(secondsLeft: Int, countdown: UILabel!, timeEdit: UITextField!) {
+        self.secondsLeft = secondsLeft
+        self.timer = Timer()
+        //self.formatter = formatter
+        self.isRunning = false
+        self.countdown = countdown
+        self.timeSpent = 0
+    }
+    
+    func setStartTime() {
+        startTime = Date()
+    }
+    
+    func startTimer() {
+        setStartTime()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true, block: runTimer(timer:))
+        isRunning = true
+        }
+    
+    func runTimer(timer : Timer) {
+        
+        if let startTime {
+            timeSpent = (Int(Date().timeIntervalSince1970) - Int(startTime.timeIntervalSince1970))
+        } else {return}
+        
+        if secondsLeft - timeSpent  > 0 {
+            print(secondsLeft)
+            countdown.text = String(secondsLeft - timeSpent)
+        } else {
+            timer.invalidate()
+            isRunning = false
+            countdown.text = "TIME'S UP"
+        }
+        
+    }
+    
+    func stopTimer() {
+        
+        secondsLeft -= timeSpent
+        //timeEdit.text = String(secondsLeft)
+        isRunning = false
+        timer?.invalidate()
+    }
+    
+}
--- chessClock/ViewController.swift
+++ chessClock/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timeEdit: UITextField!
+    
+    @IBOutlet weak var countdown: UILabel!
+    
+    //var isRunning = false
+    //var timer : Timer?
+    var secondsLeft : Int = 15
+    //var timeSpent : Int = 0
+    
+    var startTime : Date?
+    var stopTime : Date?
+    var clockOne : Clock?
+    
+    
+    
+    //let formatter = DateFormatter()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //formatter.timeStyle = .medium
+        clockOne = Clock(secondsLeft: secondsLeft, countdown: countdown, timeEdit: timeEdit)
+        
     }
-
-
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        if let clockOne {
+            if clockOne.isRunning {
+                clockOne.stopTimer()
+            } else {
+                //if let timeEditInt = Int(timeEdit.text ?? "120") {timeLeft = timeEditInt}
+                //startTime = Date()
+                //isRunning = true
+                //startTimer()
+                
+                clockOne.startTimer()
+            }
+        }
+    }
 }
 
